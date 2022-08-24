3c2b5f66ca8354b4d13afb2e39e549ccc2c36705
Agwe Bryan <agwedinobryan@gmail.com>
2022-08-25 00:29:59 +0100

    create navigation controller

--- lib/constants/controllers.dart
+++ lib/constants/controllers.dart
@@ -1,4 +1,6 @@
 // GLOBAL CONTROLLERS
 import 'package:flutter_web_app/controllers/menu_controller.dart';
+import 'package:flutter_web_app/controllers/navigation_controller.dart';
 
 final menuController = MenuController.instance;
+final navigationController = NavigationController.instance;
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_web_app/controllers/menu_controller.dart';
+import 'package:flutter_web_app/controllers/navigation_controller.dart';
 import 'package:flutter_web_app/views/screens/layout.dart';
 import 'package:get/get.dart';
 
 void main() {
   Get.put(MenuController());
+  Get.put(NavigationController());
   runApp(App());
 }
 
